b237f177d5ad873926cce0137e26901510050195
18326840 <88755937+18326840@users.noreply.github.com>
2021-08-27 21:26:17 +1000

    Update q3v2.py

--- q3v2.py
+++ q3v2.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
-from sklearn.metrics import plot_confusion_matrix
-import numpy as np
 import os
 import pandas as pd
 import matplotlib as mpl
@@ -17,11 +15,8 @@ from sklearn.model_selection import RepeatedKFold
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix
 from pandas import read_csv
 from matplotlib import pyplot
-import matplotlib.pyplot as plt
 from numpy import arange
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import RidgeCV
@@ -41,11 +36,11 @@ grid['alpha'] = arange(0, 1, 0.01)
 
 GSearch = GridSearchCV(ridge, grid, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
 results = GSearch.fit(X_train,y_train)
-print('MAE: %.3f' % results.best_score_)
-print('Config: %s' % results.best_params_)
 scores = cross_val_score(GSearch, GSearch.predict(X_test), y_test, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
 scores = absolute(scores)
-print('Mean MAE: %.3f (%.3f)' % (mean(scores), std(scores)))
+print('MAE' + results.best_score_)
+print('Config' + results.best_params_)
+print('Mean of MAE' + (mean(scores), std(scores)))
 
 
 
